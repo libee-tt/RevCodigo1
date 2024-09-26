27274447958cbbc00345f49298311f7059a0adf9
Libertad <lrivascedillo@gmail.com>
2024-09-26 12:05:57 -0600

    Se corrigieron errores de código y se agregaron funcionalidades

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,37 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+
+//correccion de selectores
+const $n = document.querySelector('.name'); //se agrego un punto para seleccionar clase
+const $b = document.querySelector('.blog'); //se agrego un punto para seleccionar clase
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
-}
+
+async function displayUser(username) { //se agregó async para que funcione await
+  $n.textContent = 'Cargando...';
+  try{ //agregado para manejar errores
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    //validación error
+    if (!response.ok){
+      throw new Error(`HTTP error! status: ${response.status}`);
+
+    }//if
+    const data = await response.json(); //obteemos datos en JSON
+    console.log(data);
+    //se quitaron '{}' de todas
+    $n.textContent = data.name;
+    $b.textContent = data.blog || 'No tiene blog'; ///manejo si no hay blog
+    $l.textContent = data.location || 'Ubicación no disponible';//manejo si no hay ubicación
+  } catch (err){
+    handleError(err); // llamado a función de manejar errores 
+  }//catch
+
+}//displayUser
 
 function handleError(err) {
-  console.log('OH NO!');
-  console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  console.error('OH NO!', err); //se cambio de console.log a error y se agregó parámetro err en la misma
+  $n.textContent = `Algo salió mal: ${err.message}`; //se utilizó message para tener el msj
 }
+
 
 displayUser('stolinski').catch(handleError);
